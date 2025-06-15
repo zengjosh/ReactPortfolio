90c1e117b5559793656ce2362e5f562d5d994eb8
zengjosh <zeng080407@gmail.com>
2025-06-15 13:19:38 -0700

    Revert "a"
    This reverts commit 2102b7dd918afb31becb3fe9de4a3354d1aca8b9.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 <<<<<<< HEAD
-<<<<<<< HEAD
   plugins: [
     react(),
   ],
@@ -30,12 +29,6 @@ export default defineConfig({
       },
     },
   },
-=======
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
->>>>>>> parent of d4bceef (fix email functionality hopefully)
 =======
   plugins: [react()],
   optimizeDeps: {
